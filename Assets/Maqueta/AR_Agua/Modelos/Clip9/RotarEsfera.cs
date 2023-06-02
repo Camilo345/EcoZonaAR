2b92b505ac72e3830b82cc87c28f1b7ca91c89f9
Daniel C <65357375+danielc1304@users.noreply.github.com>
2023-06-01 20:57:20 -0500

    Fix velocidad rotacion

--- Assets/Maqueta/AR_Agua/Modelos/Clip9/RotarEsfera.cs
+++ Assets/Maqueta/AR_Agua/Modelos/Clip9/RotarEsfera.cs
@@ -10,25 +10,31 @@ public class RotarEsfera : MonoBehaviour
     private float rotateSpeed = 1.0f;
 
     // Start is called before the first frame update
-    void Start()
+    //void Start()
+    //{
+    //    //starts the Coroutine
+    //    StartCoroutine(Spin());
+    //}
+
+    private void Update()
     {
-        //starts the Coroutine
-        StartCoroutine(Spin());
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotateSpeed);
     }
+
     //to spin the coin.All Coroutines must have a return Type IEnumerator
-    private IEnumerator Spin()
-    {
-        //to rotate smootly a bit on each frame
-        while (true)
-        {
-            //transform.Rotate => transform function from Rotate class
-            //transform.up => along y axis
-            //Time.deltaTime => how much time since last frame
-            //360 * rotateSpeed * Time.deltaTime => how much i nedd to rotate each frame
-            transform.Rotate(transform.up, 360 * rotateSpeed * Time.deltaTime);
-            // to avoid eternal loop
-            yield return 0;
-        }
-    }
+    //private IEnumerator Spin()
+    //{
+    //    //to rotate smootly a bit on each frame
+    //    while (true)
+    //    {
+    //        //transform.Rotate => transform function from Rotate class
+    //        //transform.up => along y axis
+    //        //Time.deltaTime => how much time since last frame
+    //        //360 * rotateSpeed * Time.deltaTime => how much i nedd to rotate each frame
+    //        transform.Rotate(transform.up, 360 * rotateSpeed * Time.deltaTime);
+    //        // to avoid eternal loop
+    //        yield return 0;
+    //    }
+    //}
 }
 

70a8c1e1888832d521067eecc19b6c1e1b5ce415
Daniel C <65357375+danielc1304@users.noreply.github.com>
2023-06-08 22:17:57 -0500

    Nuevas texturas maqueta agua

--- Assets/Maqueta/AR_Agua/Modelos/Clip9/RotarEsfera.cs
+++ Assets/Maqueta/AR_Agua/Modelos/Clip9/RotarEsfera.cs
@@ -3,38 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotarEsfera : MonoBehaviour
-{
-    //var to set the coin rotation spedd in Inspector
+{ 
     [SerializeField]
-    //1.0f rotattions/sec
-    private float rotateSpeed = 1.0f;
+    private float velocidadRotacion = 1.0f;
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
-    //    //starts the Coroutine
-    //    StartCoroutine(Spin());
-    //}
+    public enum Eje { x, y, z}
+
+    public Eje EjeRotacion;
+
 
     private void Update()
     {
-        transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotateSpeed);
+        switch (EjeRotacion)
+        {
+            case Eje.x:
+                transform.RotateAround(transform.position, transform.right, Time.deltaTime * velocidadRotacion);
+                break;
+            case Eje.y:
+                transform.RotateAround(transform.position, transform.up, Time.deltaTime * velocidadRotacion);
+                break;
+            case Eje.z:
+                transform.RotateAround(transform.position, transform.forward, Time.deltaTime * velocidadRotacion);
+                break;
+            default:
+                break;
+        }
     }
-
-    //to spin the coin.All Coroutines must have a return Type IEnumerator
-    //private IEnumerator Spin()
-    //{
-    //    //to rotate smootly a bit on each frame
-    //    while (true)
-    //    {
-    //        //transform.Rotate => transform function from Rotate class
-    //        //transform.up => along y axis
-    //        //Time.deltaTime => how much time since last frame
-    //        //360 * rotateSpeed * Time.deltaTime => how much i nedd to rotate each frame
-    //        transform.Rotate(transform.up, 360 * rotateSpeed * Time.deltaTime);
-    //        // to avoid eternal loop
-    //        yield return 0;
-    //    }
-    //}
 }
 
